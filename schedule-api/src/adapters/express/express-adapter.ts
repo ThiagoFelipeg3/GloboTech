9abd3189e9fc3a6d88a31548b8a43616a2011c77
Thiago Felipe <thiagof.g3@gmail.com>
2022-10-27 22:31:25 -0300

    Criando um adaptador para o express

---
+++ schedule-api/src/adapters/express/express-adapter.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { Controller } from '../../interfaces/controller'
+import {
+    HttpRequest,
+    HttpResponse
+} from '../../interfaces/http'
+
+
+export const adaptExpress = (controller: Controller) => {
+    return async (req: Request, res: Response) => {
+        const httpRequest: HttpRequest = {
+            body: req.body
+        }
+        const httpResponse: HttpResponse = await controller.handle(httpRequest)
+        const { statusCode, body } = httpResponse
+        const response = statusCode > 299 ? { error: body.message } : body
+
+        res.status(statusCode).json(response)
+    }
+}
