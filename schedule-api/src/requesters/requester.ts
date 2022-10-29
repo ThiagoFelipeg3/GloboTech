431e93b00d718c0bfb69027824e38348c7062bd0
Thiago Felipe <thiagof.g3@gmail.com>
2022-10-29 14:51:31 -0300

    Criando modelo de requisitor base para as outras requisicoes

---
+++ schedule-api/src/requesters/requester.ts
@@ -0,0 +1,30 @@
+import fetch from 'node-fetch';
+import { Method } from '../enums/methods';
+import { IRequester } from '../interfaces/requester';
+
+export abstract class Requester implements IRequester {
+    protected sufixUrl = 'esportes/futebol/modalidades/futebol_de_campo/categorias/profissional';
+
+    public async makeRequest() {
+        const options = await this.getOptions();
+        const response = await fetch(this.getUrl(), options);
+
+        return response.json();
+    }
+
+    protected generateHeaders() {
+        return {
+            'Content-type': 'application/json',
+        };
+    }
+
+    protected generateBody() {
+        return {};
+    }
+
+    protected abstract getUrl(): string;
+
+    protected abstract getOptions();
+
+    protected abstract getMethod(): Method;
+}
