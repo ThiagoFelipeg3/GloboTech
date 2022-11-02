3d67edf9a47800c01dbb58909331ddfa1675eaab
Thiago Felipe <thiagof.g3@gmail.com>
2022-11-02 15:55:19 -0300

    Formatando mais os dados agrupando por campeonato e fases

--- schedule-api/src/services/schedule-service.ts
+++ schedule-api/src/services/schedule-service.ts
@@ -22,25 +22,54 @@ export class ScheduleService implements Schedule {
                     awayTeam,
                     fase,
                     edition,
-                    championship
+                    championship,
+                    sede
                 ] = await this.getAttributes(game, referencias)
 
-                /**
-                 * Retornar uma lista com jogos dentro de cada campeonato isso vai evitar que
-                 * o cliente tenha que sempre organizar as respostas
-                 * Campeonato 1 = [jogos,jogos,jogos]
-                 * Campeonato 2 = [jogos,jogos,jogos]
-                 */
-
                 if (!accumulatorGames[championship.nome]) {
-                    accumulatorGames[championship.nome] = [];
+                    accumulatorGames[championship.nome] = {};
                 }
 
-                accumulatorGames[championship.nome].push({
-                    homeTeam,
-                    awayTeam,
-                    fase,
-                    edition
+                const formatFase = {
+                    fase_id: fase.fase_id,
+                    ordem: fase.ordem,
+                    data_inicio: fase.data_inicio,
+                    data_fim: fase.data_fim,
+                    nome: fase.nome
+                }
+
+                if (!accumulatorGames[championship.nome][fase.nome]) {
+                    accumulatorGames[championship.nome][fase.nome] = {
+                        ...formatFase,
+                        nome: edition.nome,
+                        jogos: []
+                    };
+                }
+
+                const placar: any = {};
+                placar[game.equipe_mandante_id] = game.placar_oficial_mandante;
+                placar[game.equipe_visitante_id] = game.placar_oficial_visitante;
+
+                const equipes: any = {};
+                equipes[game.equipe_mandante_id] = {
+                    nome: homeTeam.nome,
+                    apelido: homeTeam.apelido,
+                    escudos: homeTeam. escudos
+                };
+                equipes[game.equipe_visitante_id] = {
+                    nome: awayTeam.nome,
+                    apelido: awayTeam.apelido,
+                    escudos: awayTeam. escudos
+                };;
+
+                accumulatorGames[championship.nome][fase.nome]['jogos'].push({
+                    vencvencedor_jogo: game.vencedor_jogo,
+                    rodada: game.rodada,
+                    placar,
+                    suspenso: game.suspenso,
+                    data_hora_realizacao: `${game.data_realizacao} ${game.hora_realizacao}`,
+                    nome_sede: sede.nome,
+                    equipes
                 })
 
                 return Promise.resolve(accumulatorGames);
@@ -50,21 +79,43 @@ export class ScheduleService implements Schedule {
     }
 
     private async getAttributes(game: any, referencias: any): Promise<any> {
-        const { equipes, fases, edicoes, campeonatos } = referencias;
-        const { fase_id, edicao_id } = game;
+        const { equipes, fases, edicoes, campeonatos, sedes } = referencias;
 
-        const fase = fases[fase_id];
-        const edition = edicoes[edicao_id];
+        const fase = fases[game.fase_id];
+        const edition = edicoes[fase.edicao_id];
         const [ homeTeam, awayTeam ] = await this.getTeams(game, equipes);
         const championship = await this.getChampionship(edition, campeonatos);
+        const sede = sedes[game.sede_id];
 
         return [
             homeTeam,
             awayTeam,
             fase,
             edition,
-            championship
+            championship,
+            sede
         ];
+    }
+
+    private getPhaseFormat(faseId: number, fases: any) {
+        const fase = fases[faseId];
+        const {
+            fase_id,
+            edicao_id,
+            ordem,
+            data_inicio,
+            data_fim,
+            nome
+        } = fase;
+
+        return {
+            fase_id,
+            edicao_id,
+            ordem,
+            data_inicio,
+            data_fim,
+            nome
+        };
     }
 
     private async getTeams(game: any, equipes: any): Promise<any> {
@@ -83,7 +134,7 @@ export class ScheduleService implements Schedule {
         return Promise.all([
             homeTeam?.makeRequest(),
             awayTeam?.makeRequest(),
-        ])
+        ]);
     }
 
     private async getChampionship(edition: any, campeonatos: any): Promise<any> {
