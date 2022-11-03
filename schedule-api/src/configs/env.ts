b8604dbbef2a74a02d2d0c9267b5b638c9a917a3
Thiago Felipe <thiagof.g3@gmail.com>
2022-11-03 20:10:38 -0300

    Adicionando o dotenv para manupulacao de variaveis de ambiente

--- schedule-api/src/configs/env.ts
+++ schedule-api/src/configs/env.ts
@@ -1,4 +1,4 @@
 export default {
-    esportes_api: 'http://127.0.0.1:8080',
+    esportes_api: process.env.ESPORTES_API_URL,
     port: process.env.PORT || 8000,
 }
--- schedule-api/src/server.ts
+++ schedule-api/src/server.ts
@@ -1,4 +1,7 @@
 import env from './configs/env';
 import app from './configs/app';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port} process ${process.pid}`))
