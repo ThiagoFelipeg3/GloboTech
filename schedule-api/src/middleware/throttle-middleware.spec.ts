8a8c2577e64f520cfbaddae31273a577ca3e62ac
Thiago Felipe <thiagof.g3@gmail.com>
2022-11-26 18:29:09 -0300

    test: deveria retornar limitExceededError

---
+++ schedule-api/src/middleware/throttle-middleware.spec.ts
@@ -0,0 +1,45 @@
+import { limitExceededError, unauthorized } from '../helpers/http-helper';
+import { Cache } from '../interfaces/cache';
+import { ThrottleMiddleware } from '../middleware/throttle-middleware';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mockCache = () => {
+    class MockCache implements Cache {
+        public async get(key: string): Promise<any> {}
+        public set(key: string, value: any, expireInSeconds: number) {}
+        public increment(key: string): any {}
+        public async eval(
+            script: string,
+            key: string,
+            window: number,
+            limit: number,
+            numberKeys = 1
+        ): Promise<boolean> {
+            return Promise.resolve(false);
+        }
+    }
+
+    return new MockCache();
+};
+
+const makeSut = () => {
+    const cache = mockCache();
+    const sut = new ThrottleMiddleware(cache);
+
+    return {
+        sut,
+        cache
+    };
+}
+
+describe('ThrottleMiddleware Middleware', () => {
+    test('Should return 429 if it exceeds the number of requests', async () => {
+        const { sut, cache } = makeSut()
+        jest.spyOn(cache,  'eval').mockReturnValueOnce(Promise.resolve(true));
+        const httpResponse = await sut.handle()
+
+        expect(httpResponse).toEqual(limitExceededError())
+    })
+});
